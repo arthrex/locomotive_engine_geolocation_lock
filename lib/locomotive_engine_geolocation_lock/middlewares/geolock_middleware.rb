220a5308423cbcb89238fe888eb5c685150e433f
Toni Hoffmann <toni.hoffmann@arthrex.de>
2021-03-05 10:45:35 +0100

    darn

--- lib/locomotive_engine_geolocation_lock/middlewares/geolock_middleware.rb
+++ lib/locomotive_engine_geolocation_lock/middlewares/geolock_middleware.rb
@@ -14,13 +14,14 @@ module LocomotiveEngineGeolocationLock
           puts "Checking Geolocation Lock"
           lock_page_handle = 'locked-country'
           lock_page_handle = ENV['GEOLOCATION_LOCK_PAGE_HANDLE'] unless ENV['GEOLOCATION_LOCK_PAGE_HANDLE'].nil?
+        
+          request_ip = get_client_ip
+          user_country = get_country_by_ip(request_ip)
+          lock_countries = site.request_geolocation_lock_countries.gsub(/\s+/, "").downcase.split(',')
           if page.handle == lock_page_handle #or is_crawler
             puts "Checking if is on lock page but not locked country"
             redirect_to '/', 302 unless (lock_countries.include? user_country.downcase)
           else
-            request_ip = get_client_ip
-            user_country = get_country_by_ip(request_ip)
-            lock_countries = site.request_geolocation_lock_countries.gsub(/\s+/, "").downcase.split(',')
             if (lock_countries.include? user_country.downcase)
               puts "Redirecting user of locked country"
               redirect_to_page lock_page_handle , 302
